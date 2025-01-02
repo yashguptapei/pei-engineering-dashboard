08c00c2dc51dccf4f14f96cddf18495a59457ad5
Yash Gupta <yash.g@pei.group>
2025-01-02 14:22:12 +0530

    chore: update favicon
    1. update favicon in web app.
    2. fix react and react-dom types package version.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,9 @@
+import type { JSX } from "react";
+
 export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
+        <p>Home page</p>
     </main>
   );
 }
